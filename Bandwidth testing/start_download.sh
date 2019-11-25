1c4e9896b0fe5907603a7230969e5b80f593b112
Dominic Cordeiro <dominiccordeiro@gmail.com>
2019-11-25 14:47:23 +0800

    remove folders

--- Bandwidth testing/start_download.sh
+++ Bandwidth testing/start_download.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+rm -rf http3_logs
+rm -rf http2_logs
+
 mkdir http3_logs
 for i in {1..10}
 do
-    curl -o test.txt --http3 http://http3-test.litespeedtech.com:4433/10000000 2>&1 | tee "http3_logs/http3_${i}.log"
+    curl -o test.txt --http3 https://http3-test.litespeedtech.com:4433/10000000 2>&1 | tee "http3_logs/http3_${i}.log"
 done
 
 mkdir http2_logs
 for j in {1..10}
 do
-    curl -o test.txt --http2 http://http3-test.litespeedtech.com:4433/10000000 2>&1 | tee "http2_logs/http2_${j}.log"
+    curl -o test.txt --http2 https://http3-test.litespeedtech.com:4433/10000000 2>&1 | tee "http2_logs/http2_${j}.log"
 done
