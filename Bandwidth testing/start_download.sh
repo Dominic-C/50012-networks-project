7fb9a7a402f29ce13c28417ea20fd48c9e452a26
Dominic Cordeiro <dominiccordeiro@gmail.com>
2019-11-25 14:32:15 +0800

    modified scritps

--- Bandwidth testing/start_download.sh
+++ Bandwidth testing/start_download.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+mkdir http3_logs
 for i in {1..10}
 do
-    curl -o test.txt --http3 http://http3-test.litespeedtech.com:4433/ 2>&1 | tee "http3_${i}.log"
+    curl -o test.txt --http3 http://http3-test.litespeedtech.com:4433/ 2>&1 | tee "http3_logs/http3_${i}.log"
     rm test.txt
 done
 
+mkdir http2_logs
 for j in {1..10}
 do
-    curl -o test.txt --http2 http://http3-test.litespeedtech.com:4433/ 2>&1 | tee "http3_${j}.log"
+    curl -o test.txt --http2 http://http3-test.litespeedtech.com:4433/ 2>&1 | tee "http2_logs/http2_${j}.log"
     rm test.txt
 done
